079dba4eab0bf58dba7111cf16831678e6a16c55
Alexander Gurin <alexandergurin1996@gmail.com>
2017-12-09 11:06:20 +0300

    Добавид команду и проверку "открыт ли диск"

--- tkOS/tkOS/Program.cs
+++ tkOS/tkOS/Program.cs
@@ -8,17 +8,22 @@ using FileSystem;
 namespace tkOS {
     class Program {
         static void Main(string[] args) {
-            FileSystem.FileSystem disk = new FileSystem.FileSystem(1024);
+            FileSystem.FileSystem disk = null;
             while (true) {
                 Console.Write(">");
                 string key = Console.ReadLine();
-                switch (key.Split(' ')[0]) {
-                    case "ListFile": Console.WriteLine(disk.GetListFiles()); break;
-                    case "CreateFile": disk.CreateFile(key.Split(' ')[1]); break;
-                    case "DeleteFile": disk.DeleteFile(key.Split(' ')[1]); break;
-                    case "RenameFile": disk.Rename(key.Split(' ')[1], key.Split(' ')[2]); break;
-                    case "WriteFile": disk.WriteData(key.Split(' ')[1], key.Split(' ')[2]); break;
-                    case "ReadFile": Console.WriteLine(disk.ReadData(key.Split(' ')[1], false)); break;
+                try {
+                    switch (key.Split(' ')[0]) {
+                        case "CreateDisk": disk = new FileSystem.FileSystem(1024); break;
+                        case "ListFile": Console.WriteLine(disk.GetListFiles()); break;
+                        case "CreateFile": disk.CreateFile(key.Split(' ')[1]); break;
+                        case "DeleteFile": disk.DeleteFile(key.Split(' ')[1]); break;
+                        case "RenameFile": disk.Rename(key.Split(' ')[1], key.Split(' ')[2]); break;
+                        case "WriteFile": disk.WriteData(key.Split(' ')[1], key.Split(' ')[2]); break;
+                        case "ReadFile": Console.WriteLine(disk.ReadData(key.Split(' ')[1], false)); break;
+                    }
+                } catch(Exception e) {
+                    if (disk == null) Console.WriteLine("Подключите диск!");
                 }
             }
         }
